3db65df3d2c51e29f6de80a545c6aef95cf313e5
rferra4 <36964053+rferra4@users.noreply.github.com>
2020-04-05 18:04:31 -0500

    Update project_schema.sql

--- project_schema.sql
+++ project_schema.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Mar 09, 2020 at 10:56 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `project_schema`
 --
@@ -29,8 +11,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `Applicants` (
   `Applicants_ID` int(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Last name` varchar(50) NOT NULL,
-  `Resume` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Resume` varchar(255) NOT NULL,
   `StatusOfApplication` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -43,9 +25,9 @@ CREATE TABLE `Applicants` (
 CREATE TABLE `Association` (
   `Association_ID` int(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Description` varchar(100) NOT NULL,
+  `Description` varchar(255) NOT NULL,
   `Address` varchar(50) NOT NULL,
-  `id_number` int(11) NOT NULL
+  `ID_Number` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -58,10 +40,18 @@ CREATE TABLE `Company` (
   `ID_Number` int(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Address` varchar(50) NOT NULL,
-  `Description` varchar(100) NOT NULL,
+  `Description` varchar(255) NOT NULL,
   `Employee_ID` int(50) NOT NULL,
   `JobOpening_ID` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Company`
+--
+
+INSERT INTO `Company` (`ID_Number`, `Name`, `Address`, `Description`, `Employee_ID`, `JobOpening_ID`) VALUES
+(1, 'LSU ', 'BR, LA', 'University in Baton Rouge', 1, '1'),
+(2, 'IBM', 'BR,LA', 'Entry Level Programmer', 2, '2');
 
 -- --------------------------------------------------------
 
@@ -72,7 +62,7 @@ CREATE TABLE `Company` (
 CREATE TABLE `Employee` (
   `Employee_ID` int(50) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Last name` int(50) NOT NULL,
+  `Last_Name` int(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Address` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -80,26 +70,34 @@ CREATE TABLE `Employee` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Job Opening`
+-- Table structure for table `Job_Opening`
 --
 
-CREATE TABLE `Job Opening` (
+CREATE TABLE `Job_Opening` (
   `Opening_ID` int(50) NOT NULL,
-  `Description` varchar(50) NOT NULL,
+  `Description` varchar(255) NOT NULL,
   `Date` date NOT NULL,
   `Status` varchar(50) NOT NULL,
-  `Company ID` int(50) NOT NULL,
+  `Company_ID` int(50) NOT NULL,
   `Applicants_ID` int(50) NOT NULL,
   `SearchCommittee_ID` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `Job_Opening`
+--
+
+INSERT INTO `Job_Opening` (`Opening_ID`, `Description`, `Date`, `Status`, `Company_ID`, `Applicants_ID`, `SearchCommittee_ID`) VALUES
+(1, 'Teaching assistant position', '2020-04-05', 'open', 2, 2, 2),
+(2, 'Design Websites', '2020-04-05', 'open', 1, 1, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Search Committee`
+-- Table structure for table `Search_Committee`
 --
 
-CREATE TABLE `Search Committee` (
+CREATE TABLE `Search_Committee` (
   `SearchCommittee_ID` int(50) NOT NULL,
   `Employee_ID` int(50) NOT NULL,
   `Opening_ID` int(50) NOT NULL
@@ -134,17 +132,53 @@ ALTER TABLE `Employee`
   ADD PRIMARY KEY (`Employee_ID`);
 
 --
--- Indexes for table `Job Opening`
+-- Indexes for table `Job_Opening`
 --
-ALTER TABLE `Job Opening`
+ALTER TABLE `Job_Opening`
   ADD PRIMARY KEY (`Opening_ID`);
 
 --
--- Indexes for table `Search Committee`
+-- Indexes for table `Search_Committee`
 --
-ALTER TABLE `Search Committee`
+ALTER TABLE `Search_Committee`
   ADD PRIMARY KEY (`SearchCommittee_ID`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Applicants`
+--
+ALTER TABLE `Applicants`
+  MODIFY `Applicants_ID` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Association`
+--
+ALTER TABLE `Association`
+  MODIFY `Association_ID` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Company`
+--
+ALTER TABLE `Company`
+  MODIFY `ID_Number` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `Employee`
+--
+ALTER TABLE `Employee`
+  MODIFY `Employee_ID` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Job_Opening`
+--
+ALTER TABLE `Job_Opening`
+  MODIFY `Opening_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+
+--
+-- AUTO_INCREMENT for table `Search_Committee`
+--
+ALTER TABLE `Search_Committee`
+  MODIFY `SearchCommittee_ID` int(50) NOT NULL AUTO_INCREMENT;
