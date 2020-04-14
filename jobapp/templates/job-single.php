1cf55304710255c0309411d1df45dd16885b73d8
rferra4 <rferra4@lsu.edu>
2020-04-14 01:38:51 -0500

    create resume upload

--- jobapp/templates/job-single.php
+++ jobapp/templates/job-single.php
@@ -1,8 +1,25 @@
 <?php include 'inc/header.php'; ?>
+<!DOCTYPE html>
+<html>
 <div class="jumbotron">
   <h1 class="display-4">Job Details</h1>
 
 </div>
+
+
+<head>
+<title>Upload your files</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+  <p>Upload your file</p>
+  <input type="file" name="file"></input><br />
+  <input type="submit" value="Upload" name = "upload"></input>
+</form>
+</body>
+</html>
+
+
 
 <?php echo $job->Title; ?>
 <br>
