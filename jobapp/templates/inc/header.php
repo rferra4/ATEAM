37f3efef38484fc23e55fb21c5d6f4c5d22214a4
rferra4 <rferra4@lsu.edu>
2020-04-13 21:50:53 -0500

    test

--- jobapp/templates/inc/header.php
+++ jobapp/templates/inc/header.php
@@ -10,7 +10,7 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills float-right">
-            <li class="nav-item"><a class="nav-link active" href="index.php">Home </a></li>
+            <li class="nav-item"><a class="nav-link active" href="index.php">test </a></li>
             <li class="nav-item"><a class="nav-link" href="create.php">Create Listing</a></li>
             <li class="nav-item"><a class="nav-link" href="registration.php">Sign Up</a></li>
             <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
