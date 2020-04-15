a655d55f1d1a956a9fdba9a943261b65e677a701
rferra4 <rferra4@lsu.edu>
2020-04-14 22:41:17 -0500

    redirect to login first

--- jobapp/index.php
+++ jobapp/index.php
@@ -1,22 +1,2 @@
 <?php include_once 'config/init.php'; ?>
-
-<?php
-$job = new Job;
-
-$template = new Template('templates/view_listings.php');
-
-$category = isset($_GET['category']) ? $_GET['category'] : null;
-
-if($category){
-    $template->Job_Opening = $job->getByCategory($category);
-
-
-}else{
-    $template->title = 'Latest Jobs ';
-    $template->Job_Opening = $job->getAllJobs();
-}
-
-
-$template->Company = $job->getCategories();
-
-echo $template;
+<?php include 'login.php'; ?>
---
+++ jobapp/viewer.php
@@ -0,0 +1,22 @@
+<?php include_once 'config/init.php'; ?>
+
+<?php
+$job = new Job;
+
+$template = new Template('templates/view_listings.php');
+
+$category = isset($_GET['category']) ? $_GET['category'] : null;
+
+if($category){
+    $template->Job_Opening = $job->getByCategory($category);
+
+
+}else{
+    $template->title = 'Latest Jobs ';
+    $template->Job_Opening = $job->getAllJobs();
+}
+
+
+$template->Company = $job->getCategories();
+
+echo $template;
