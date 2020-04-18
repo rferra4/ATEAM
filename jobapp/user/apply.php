f63d2a6c47db03611d183eb603a8368e5d53c809
rferra4 <36964053+rferra4@users.noreply.github.com>
2020-04-18 14:53:40 -0500

    Update apply.php

--- jobapp/user/apply.php
+++ jobapp/user/apply.php
@@ -1,30 +1,26 @@
-<!DOCTYPE html>
+<<?php include 'inc/uheader.php'; ?>
+
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="../css/styles.css"/>
 </head>
+
+
+
+
+
 <body>
-
-
-<div class="hero">
-  <div class="form-box">
-    <div class="button-box">
-                <div id="btn"></div>
-
-            </div>
 
 <form id = "APPLY" class = "input-group" method="post" >
 <h3 class="input-field-title">Application</h3>
 
- <input type="text" name="first_name" class="input-field" placeholder="enter username" />
- <input type="text" name="last_name" class="input-field" placeholder="enter username" />
- <input type="text" name="txt_username" class="input-field" placeholder="enter username" />
- <input type="text" name="txt_username" class="input-field" placeholder="enter username" />
+ <input type="text" name="first_name" class="input-field" placeholder="First Name" />
+ <input type="text" name="last_name" class="input-field" placeholder="Last Name" />
+ <input type="text" name="txt_username" class="input-field" placeholder="Username" />
 
 
- <input type="text" name="txt_email" class="input-field" placeholder="enter email" />
+ <input type="text" name="txt_email" class="input-field" placeholder="Email" />
 
  <input type="password" name="skills" class="input-field" placeholder="List of skills seperated by a comma" />
 
@@ -36,7 +32,7 @@
   </select>
 
 
- <input type="submit"  name="btn_apply" class="submit-btn" value="Application">
+ <input type="submit"  name="btn_apply" class="submit-btn" value="Submit">
 
  <p class = "link"> Already applied? <a href="viewer.php">Go back to job listings here</a></p>
 </div>
@@ -44,5 +40,6 @@
 
 </form>
 </body>
+
 
 </html>
