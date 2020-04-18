5c73c26a2cb44d8fe4229c17ffab3ffe22aee31f
rferra4 <36964053+rferra4@users.noreply.github.com>
2020-04-18 17:10:08 -0500

    apply.php adds user info to database

--- jobapp/user/apply.php
+++ jobapp/user/apply.php
@@ -1,5 +1,4 @@
-<<?php include 'inc/uheader.php'; ?>
-
+<?php include 'inc/uheader.php'; ?>
 <html>
 <head>
     <meta charset="utf-8"/>
@@ -10,36 +9,26 @@
 
 
 
+
 <body>
-
-<form id = "APPLY" class = "input-group" method="post" >
-<h3 class="input-field-title">Application</h3>
-
- <input type="text" name="first_name" class="input-field" placeholder="First Name" />
- <input type="text" name="last_name" class="input-field" placeholder="Last Name" />
- <input type="text" name="txt_username" class="input-field" placeholder="Username" />
-
-
- <input type="text" name="txt_email" class="input-field" placeholder="Email" />
-
- <input type="password" name="skills" class="input-field" placeholder="List of skills seperated by a comma" />
-
-
-  <select class="input-field" name="txt_role">
-   <option value="" selected="selected"> - select role - </option>
-   <option value="employee">Employee</option>
-   <option value="user">User</option>
-  </select>
-
-
- <input type="submit"  name="btn_apply" class="submit-btn" value="Submit">
-
- <p class = "link"> Already applied? <a href="viewer.php">Go back to job listings here</a></p>
-</div>
-</div>
-
+<form action="insert.php" method="post">
+    <p>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="first_name" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="last_name" id="lastName">
+    </p>
+    <p>
+        <label for="emailAddress">Email Address:</label>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+    <input type="submit" value="Submit">
 </form>
 </body>
+
+
 
 
 </html>
---
+++ jobapp/user/insert.php
@@ -0,0 +1,26 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "root", "project_schema");
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Escape user inputs for security
+$first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
+$last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+
+// Attempt insert query execution
+$sql = "INSERT INTO Applicants (first_name, last_name, email) VALUES ('$first_name', '$last_name', '$email')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
+?>
