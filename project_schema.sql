a4a5f7b91d724faa686ee7dfdfbeea16d119407a
rferra4 <36964053+rferra4@users.noreply.github.com>
2020-04-18 22:38:23 -0500

    Update project_schema.sql

--- project_schema.sql
+++ project_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 19, 2020 at 01:08 AM
+-- Generation Time: Apr 19, 2020 at 03:37 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -33,10 +33,28 @@ CREATE TABLE `Applicants` (
 --
 
 INSERT INTO `Applicants` (`Applicants_ID`, `first_name`, `last_name`, `email`, `jobID`) VALUES
+(1, 'Rachel', 'Ferrara', '', 0),
+(2, 'sally', 'smith', '', 0),
+(3, 'john', 'smith ', '', 0),
+(4, '', '', '', 0),
+(5, '', '', '', 0),
+(6, 'j', 'j', 'j@gmail.com', 0),
+(7, 'j', 'j', 'j@gmail.com', 0),
+(8, 'k', 'k', 'k', 0),
+(9, 'r', 'r', 'r', 0),
+(10, 'o', 'o', 'o', 0),
+(11, 'p', 'p', 'p', 0),
+(12, 'q', 'q', 'q', 0),
+(13, 't', 't', 't', 0),
+(14, 'q', 'q', 'q', 0),
+(15, 'n', 'n', 'n', 0),
 (16, 'd', 'd', 'd', 12),
 (17, 't', 't', 't', 12),
+(18, 'uu', 'u', 'u', 2),
 (19, 'c', 'c', 'c', 12),
-(20, 'test', 'test', 'test@gmail.com', 11);
+(20, 'test', 'test', 'test@gmail.com', 11),
+(21, 't', 't', 't', 12),
+(22, 'hi', 'hi', 'hi@gmail.com', 24);
 
 -- --------------------------------------------------------
 
@@ -124,23 +142,25 @@ INSERT INTO `Employee` (`Employee_ID`, `username`, `password`, `First_Name`, `La
 CREATE TABLE `Job_Opening` (
   `Opening_ID` int(50) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `Description` varchar(5000) NOT NULL,
-  `Date` date NOT NULL,
-  `Status` varchar(50) NOT NULL,
-  `Company_ID` int(50) NOT NULL,
-  `Applicants_ID` int(50) NOT NULL,
-  `SearchCommittee_ID` int(50) NOT NULL
+  `Description` varchar(10000) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `recruiter_email` varchar(50) NOT NULL,
+  `salary` int(50) NOT NULL,
+  `Company_Name` varchar(100) NOT NULL,
+  `location` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `Job_Opening`
 --
 
-INSERT INTO `Job_Opening` (`Opening_ID`, `Title`, `Description`, `Date`, `Status`, `Company_ID`, `Applicants_ID`, `SearchCommittee_ID`) VALUES
-(11, 'Software Engineer', 'Basic Qualifications\r\n• Master\'s Degree in Computer Science or related field \r\n• Expert knowledge of one of the following programming languages: Java, C and C++ \r\n• 5+ years of hands on experience in software development, including design, implementation, debugging, and support, building scalable system software and/or Services \r\n• Experience working with REST and RPC service patterns and other client/server interaction models \r\nPreferred Qualifications\r\n• Master Degree or PhD in Computer Science, Computer Engineering or related field. \r\n• Experience with BigData technology e.g. Hadoop, and Spark \r\n• iOS and Android SDK experience \r\n• Track record of building and delivering mission critical, 24x7 production software systems \r\n• Deep understanding of distributed systems and web services technology \r\n• Strong at applying data structures, algorithms, and object-oriented design, to solve challenging problems\r\n', '2020-01-10', 'open to applicants', 1, 1, 1),
-(12, 'Senior Developer', 'Basic Qualifications\r\n• Master’s or Ph.D in Computer Science \r\n• Development, Maintenance, and Troubleshooting in Google Ad Manager (formerly DFP)\r\n• Development and Maintenance of Tools for enhanced trafficking, automation, and Creative previews \r\n• Develop, Debug next level Ad Products that serve on our any of our platforms, on- and off property \r\n• Collaborate with and execute major cross-platform executions as a team, or independently when needed \r\n• Document knowledge and processes specific as it correlates to your work. \r\n• Utilize strong interpersonal skills in working with numerous internal teams and expand personal and team knowledge of new and upcoming products. \r\nPreferred Qualifications\r\n• Ph.D degree preferred, or relevant experience \r\n• A portfolio or links to previous work \r\n• 7 years of experience working in a technical service environment preferably supporting mobile or web-based products \r\n• Demonstrated creative problem-solving and strong analytical skills required \r\n• Demonstrated mastery of Object-Oriented JavaScript, CSS and HTML5 is required \r\n• Experience with Web-based languages, including React.JS and Angular \r\n• Experience with Mobile App-based languages: MRAID is a requirement, Swift and Java are a plus \r\n• Creative experience with Adobe Creative Cloud is a plus \r\n• Working knowledge of Machine Learning & AI is a plus \r\n• Working knowledge of Node.js, Grunt, Gulp, Git, WordPress, and Drupal is a plus \r\n• Strong working knowledge with one or more Internet ad management or targeted marketing applications (e.g., GAM/DFP, 24/7 Real Media, OAS, FreeWheel) \r\n• Demonstrated knowledge of the Programmatic Advertising landscape (Adform, AppNexus, Beeswax, and other DSPs) \r\n• Rich Media Vendor technology experience (e.g., Sizmek, Celtra, Pointroll) \r\n• A passion for creative technology and new media capabilities for advertising \r\n• Ability to absorb complex technical concepts and communicate them to a non-technical audience \r\n• Strong creative, collaboration and communication skills\r\n', '2020-03-10', 'open to applicants', 1, 1, 1),
-(21, 'Civil engineer', 'Basic Qualifications\r\n• BS degree in civil engineering. \r\n• 7+ years of structural engineering experience, preferably in a heavy industrial or similar field. Master’s degree and PhD in civil engineering or related field can each be substituted for 1 year of experience. \r\n• Working knowledge of structural analysis software, preferably RISA 3D Primary \r\nPreferred Qualifications\r\n• Intimate knowledge of all design codes related to design and construction of steel and concrete, including but not limited to ASCE 7, AISC Steel Construction Manual, AISC 360, ACI 318, IBC, NFPA Life Safety Code, and applicable OSHA laws & regulations. \r\n• Ability to produce accurate engineering estimates, material takeoff estimates, and schedules. \r\n• Ability to navigate and utilize 3D modeling and point cloud laser scan software. \r\n• Ability to effectively lead and manage projects of various sizes and scope, ensuring that accurate, high quality deliverables are produced on schedule and on budget. \r\n• Demonstrate effective communication and interpersonal skills, with the ability to lead a team, delegate tasks, coordinate with other disciplines, and relate with clients. \r\n• Ability to effectively and efficiently mentor less experienced engineers and designers.\r\n', '2020-01-10', 'open to applicants ', 2, 1, 1),
-(22, 'Project Engineer', 'Job Requirements Our client needs an Engineer with project management and experience in all project phases; Definition, Design, Procurement, Construction, and Start-up/Commissioning. In project execution this Project Manager will manage and lead 6-7 projects per year ranging from $200K to $2MM in scope. \r\nBasic Qualifications\r\n• Degree in engineering is required. \r\n• Strong knowledge of process equipment and their functions. \r\n• Knowledge of the different engineering discipline roles. \r\n• Proficient in project management processes. \r\n• Broad technical experience. \r\n• Experience with problem solving / troubleshooting and data analysis. \r\n• Ability to read P&ID’s and instrument loops sheets. \r\n• Experienced in commissioning of new projects within a chemical process. \r\n• Experienced in troubleshooting process and control issues in a fast paced environment. \r\n• Proficiency in Microsoft applications, such as Word and Excel. \r\n• Strong written and verbal communication skills. \r\n• Strong skill set in organization and prioritizing projects and work load. \r\n• Must be a self-starter, who is highly motivated, able to take the initiative on projects, and able to work without supervision. \r\n• Ability to follow processes and procedures, as well as develop and implement them. \r\n• The position requires strong attention to detail, as well as the ability to sit in front of a computer. \r\n• This position requires an employee to be in an office environment as well as exposed to some outside elements; process and warehouse environment. \r\nKey Words: process engineer, chemical design engineer, chemical engineer, development engineer, controls engineer, instrumentation engineer, chemical engineering, process engineer, process control engineer, process development engineer, process controls engineer, instrumentation engineer, chemical engineering, process engineer, process control engineer, ChE, petrochemical, refinery, oil and gas, pharmaceutical, process plant, operations, maintenance\r\n', '2020-01-24', 'open to applicants ', 2, 1, 1);
+INSERT INTO `Job_Opening` (`Opening_ID`, `Title`, `Description`, `Date`, `recruiter_email`, `salary`, `Company_Name`, `location`) VALUES
+(11, 'Software Engineer', 'Basic Qualifications\r\n• Master\'s Degree in Computer Science or related field \r\n• Expert knowledge of one of the following programming languages: Java, C and C++ \r\n• 5+ years of hands on experience in software development, including design, implementation, debugging, and support, building scalable system software and/or Services \r\n• Experience working with REST and RPC service patterns and other client/server interaction models \r\nPreferred Qualifications\r\n• Master Degree or PhD in Computer Science, Computer Engineering or related field. \r\n• Experience with BigData technology e.g. Hadoop, and Spark \r\n• iOS and Android SDK experience \r\n• Track record of building and delivering mission critical, 24x7 production software systems \r\n• Deep understanding of distributed systems and web services technology \r\n• Strong at applying data structures, algorithms, and object-oriented design, to solve challenging problems\r\n', '2020-01-10 06:00:00', '1', 0, '', ''),
+(12, 'Senior Developer', 'Basic Qualifications\r\n• Master’s or Ph.D in Computer Science \r\n• Development, Maintenance, and Troubleshooting in Google Ad Manager (formerly DFP)\r\n• Development and Maintenance of Tools for enhanced trafficking, automation, and Creative previews \r\n• Develop, Debug next level Ad Products that serve on our any of our platforms, on- and off property \r\n• Collaborate with and execute major cross-platform executions as a team, or independently when needed \r\n• Document knowledge and processes specific as it correlates to your work. \r\n• Utilize strong interpersonal skills in working with numerous internal teams and expand personal and team knowledge of new and upcoming products. \r\nPreferred Qualifications\r\n• Ph.D degree preferred, or relevant experience \r\n• A portfolio or links to previous work \r\n• 7 years of experience working in a technical service environment preferably supporting mobile or web-based products \r\n• Demonstrated creative problem-solving and strong analytical skills required \r\n• Demonstrated mastery of Object-Oriented JavaScript, CSS and HTML5 is required \r\n• Experience with Web-based languages, including React.JS and Angular \r\n• Experience with Mobile App-based languages: MRAID is a requirement, Swift and Java are a plus \r\n• Creative experience with Adobe Creative Cloud is a plus \r\n• Working knowledge of Machine Learning & AI is a plus \r\n• Working knowledge of Node.js, Grunt, Gulp, Git, WordPress, and Drupal is a plus \r\n• Strong working knowledge with one or more Internet ad management or targeted marketing applications (e.g., GAM/DFP, 24/7 Real Media, OAS, FreeWheel) \r\n• Demonstrated knowledge of the Programmatic Advertising landscape (Adform, AppNexus, Beeswax, and other DSPs) \r\n• Rich Media Vendor technology experience (e.g., Sizmek, Celtra, Pointroll) \r\n• A passion for creative technology and new media capabilities for advertising \r\n• Ability to absorb complex technical concepts and communicate them to a non-technical audience \r\n• Strong creative, collaboration and communication skills\r\n', '2020-03-10 05:00:00', '1', 0, '', ''),
+(21, 'Civil engineer', 'Basic Qualifications\r\n• BS degree in civil engineering. \r\n• 7+ years of structural engineering experience, preferably in a heavy industrial or similar field. Master’s degree and PhD in civil engineering or related field can each be substituted for 1 year of experience. \r\n• Working knowledge of structural analysis software, preferably RISA 3D Primary \r\nPreferred Qualifications\r\n• Intimate knowledge of all design codes related to design and construction of steel and concrete, including but not limited to ASCE 7, AISC Steel Construction Manual, AISC 360, ACI 318, IBC, NFPA Life Safety Code, and applicable OSHA laws & regulations. \r\n• Ability to produce accurate engineering estimates, material takeoff estimates, and schedules. \r\n• Ability to navigate and utilize 3D modeling and point cloud laser scan software. \r\n• Ability to effectively lead and manage projects of various sizes and scope, ensuring that accurate, high quality deliverables are produced on schedule and on budget. \r\n• Demonstrate effective communication and interpersonal skills, with the ability to lead a team, delegate tasks, coordinate with other disciplines, and relate with clients. \r\n• Ability to effectively and efficiently mentor less experienced engineers and designers.\r\n', '2020-01-10 06:00:00', '1', 0, '', ''),
+(22, 'Project Engineer', 'Job Requirements Our client needs an Engineer with project management and experience in all project phases; Definition, Design, Procurement, Construction, and Start-up/Commissioning. In project execution this Project Manager will manage and lead 6-7 projects per year ranging from $200K to $2MM in scope. \r\nBasic Qualifications\r\n• Degree in engineering is required. \r\n• Strong knowledge of process equipment and their functions. \r\n• Knowledge of the different engineering discipline roles. \r\n• Proficient in project management processes. \r\n• Broad technical experience. \r\n• Experience with problem solving / troubleshooting and data analysis. \r\n• Ability to read P&ID’s and instrument loops sheets. \r\n• Experienced in commissioning of new projects within a chemical process. \r\n• Experienced in troubleshooting process and control issues in a fast paced environment. \r\n• Proficiency in Microsoft applications, such as Word and Excel. \r\n• Strong written and verbal communication skills. \r\n• Strong skill set in organization and prioritizing projects and work load. \r\n• Must be a self-starter, who is highly motivated, able to take the initiative on projects, and able to work without supervision. \r\n• Ability to follow processes and procedures, as well as develop and implement them. \r\n• The position requires strong attention to detail, as well as the ability to sit in front of a computer. \r\n• This position requires an employee to be in an office environment as well as exposed to some outside elements; process and warehouse environment. \r\nKey Words: process engineer, chemical design engineer, chemical engineer, development engineer, controls engineer, instrumentation engineer, chemical engineering, process engineer, process control engineer, process development engineer, process controls engineer, instrumentation engineer, chemical engineering, process engineer, process control engineer, ChE, petrochemical, refinery, oil and gas, pharmaceutical, process plant, operations, maintenance\r\n', '2020-01-24 06:00:00', '1', 0, '', ''),
+(23, 'u', 'u', '2020-04-19 03:26:28', 'u@gmail.com', 90, 'PDVSA', 'u'),
+(24, 'test', 'test', '2020-04-19 03:29:48', 'test@gmail.com', 80, 'PDVSA', 'test');
 
 -- --------------------------------------------------------
 
@@ -250,7 +270,7 @@ ALTER TABLE `Search_Committee`
 -- AUTO_INCREMENT for table `Applicants`
 --
 ALTER TABLE `Applicants`
-  MODIFY `Applicants_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `Applicants_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `Association`
@@ -274,7 +294,7 @@ ALTER TABLE `Employee`
 -- AUTO_INCREMENT for table `Job_Opening`
 --
 ALTER TABLE `Job_Opening`
-  MODIFY `Opening_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Opening_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `masterlogin`
