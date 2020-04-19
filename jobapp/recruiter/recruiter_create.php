f743e182535c5e70ef665c541681aafb6daa5c35
oroone <48426379+oroone@users.noreply.github.com>
2020-04-18 21:55:12 -0500

    Recruiter Pages
    - added partial page for recruiters to view appicants with an option to send reminders about interviews
    - added rstyle.css for recruiter pages
    - added pages for recruiters to create listings

--- jobapp/recruiter/recruiter_create.php
+++ jobapp/recruiter/recruiter_create.php
@@ -1,35 +1,32 @@
 <?php include 'inc/rheader.php'; ?>
-<center>
- <h1>Recruiter CREATE LISTINGS Page</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Create Listing</title>
+    <link rel="stylesheet" href="rstyle.css"/>
 
- <h3>
-   <?php include 'inc/rfooter.php'; ?>
- <?php
-  session_start();
+<div class="hero">
+  <div class="form-box">
+    <div class="input-field-title" style="width:100%">
+              <h3>Create a Job Listing</h3>
+    </div>
 
-  if(!isset($_SESSION['admin_login'])) //check unauthorize user not direct access in "admin_home.php" page
-  {
-   header("location: ../index.php");
-  }
+      <form id ="CREATE" class="input-group" method="post"  >
+        <input placeholder="Company" type="text" name="txt_username" class="input-field"  />
+        <input placeholder="Address" type="text" name="txt_username" class="input-field"  />
+        <input placeholder="Job Title" type="text" name="txt_username" class="input-field"  />
 
-  if(isset($_SESSION['employee_login'])) //check employee login user not access in "admin_home.php" page
-  {
-   header("location: ../employee/employee_home.php");
-  }
+        <label for="job_desc" class="text-area1">Job Description:</label>
+        <textarea id="job_desc" class="text-area2"></textarea>
+        <label for="basic_qual" class="text-area1">Basic Qualifications:</label>
+        <textarea id="basic_qual" class="text-area2"></textarea>
+        <label for="pref_qual" class="text-area1">Preferred Qualifications:</label>
+        <textarea id="pref_qual" class="text-area2"></textarea>
 
-  if(isset($_SESSION['user_login'])) //check user login user not access in "admin_home.php" page
-  {
-   header("location: ../user/user_home.php");
-  }
 
-  if(isset($_SESSION['admin_login']))
-  {
-  ?>
-   Welcome,
-  <?php
-   echo $_SESSION['admin_login'];
-  }
-  ?>
- </h3>
-  <a href="../logout.php">Logout</a>
-</center>
+      </form>
+
+
+</body>
+</html>
